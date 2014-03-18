4e748991368a4984e31bbf56bc140a470590198a
Delmas_Rémi <remi.delmas@etu.u-bordeaux1.fr>
2014-03-18 01:11:47 +0100

    Premiere correction de bug, probleme sur parsing display, ça devrais O.K demain.

--- Input.c
+++ Input.c
@@ -14,11 +14,11 @@ static void free_erreur(FILE* file, char* line_ptr);
 static void affiche_erreur();
 static bool verif_champ_entree(char* ligne_ptr, const char* champ_attendu, const char* pattern);
 static bool est_valide(bool flag,FILE* imput_file,char* ligne_ptr,const char* pattern,bool erreur_possible,const char* chaine_attendu,const char* message_erreur);
-static char* alloc_chaine(int taille_ligne,int taille_pattern,char* ligne_lue);
+static char* alloc_chaine(ssize_t taille_ligne,int taille_pattern,char* ligne_lue);
 static void parsing_champs(FILE* file);
 static void affectation_display_data(int indice,double x,double y);
-static void parsing_display(FILE* file,char** ligne_ptr,size_t* taille_alloc);
-static void parsing_matrice(FILE* nom_file,char** ligne_ptr,size_t* taille_alloc);
+static void parsing_display(FILE* file,char* ligne_ptr,size_t taille_alloc);
+static void parsing_matrice(FILE* nom_file,char* ligne_ptr,size_t taille_alloc);
 
 
 /* Nous déclarons et on initialise cette structure, qui sera accessible de l'extérieur à travers des 
@@ -58,7 +58,7 @@ void print_input_data(){
 	  }
       }
     else
-      printf("NULL\n"); // Si matrice vide
+      printf("Matrice vide.\n"); // Si matrice vide
     printf("display data :\n");
     if(input.display_data != NULL)
       {
@@ -66,7 +66,8 @@ void print_input_data(){
             printf("%d -> %lf\t%lf\n", i+1, input.display_data[i][0], input.display_data[i][1]);
       }
     else
-      printf("NULL\n"); // Si matrice vide
+      printf("Matrice vide.\n"); // Si matrice vide
+    printf("\n");
 }
 
 void open_TSP_file(char* nom_file)
@@ -158,131 +159,130 @@ static bool est_valide(bool flag,FILE* imput_file,char* ligne_ptr,const char* pa
     }
     return true;
   }
-  return true;
+  return false;
 }
 
 
-static char* alloc_chaine(int taille_ligne,int taille_pattern,char* ligne_lue)
+static char* alloc_chaine(ssize_t taille_ligne,int taille_pattern,char* ligne_lue)
 {
-  /* getline renvoie la longueur de la ligne sans '/0' ,d'où le +1*/
-  char* alloc = malloc(sizeof(char)*(taille_ligne+1-taille_pattern));
+  char* alloc = malloc(sizeof(char) * (taille_ligne-taille_pattern));
   strcpy(alloc,ligne_lue+taille_pattern);
+  *(alloc+taille_ligne-taille_pattern-1)='\0'; /* On supprime le retour chariot */
   return alloc;
 }
 
 
 static void parsing_champs(FILE* file)
 {     
-  int taille_ligne = 0;
+  ssize_t taille_ligne;
   bool bool_section; // Permet d'éco. un strcmp.
+  char* ligne_ptr = NULL;
+  size_t taille_alloc = 0; /* correspond a un entier signé.*/
 
-  char ** ligne_ptr = NULL;
-  size_t * taille_alloc = NULL; /* correspond a un entier signé.*/
-  while((taille_ligne=getline(ligne_ptr, taille_alloc, file)) != EOF)/* EOF généralement égal a -1*/
+  while((taille_ligne=getline(&ligne_ptr,&taille_alloc,file)) != EOF)/* EOF généralement égal a -1*/
   {
     /* On verifie sur chaque ligne si des paramètres valides sont insérer
      * si true, on parse.
      * On utilise la fonction est_valide surtout pour des raisons de lisibilité,
      * Une fonction vraiment générique étant difficilement réalisable. */
     
-    printf("Je rentre");
     /*------- NAME -------*/
-    if(est_valide(input.nom,file,*ligne_ptr,"NAME: %42",false,NULL,NULL))
+    if(est_valide(!input.nom,file,ligne_ptr,"NAME: %42",false,NULL,NULL))
     {
       /* On pourrait allouer directement la mémoire avec sscanf, mais le code est peu-etre
        * plus lisible en passant par alloc_chaine*/
-      input.nom=alloc_chaine(taille_ligne,6,*ligne_ptr);
-      if(!strcmp('\0',input.nom))
+      input.nom=alloc_chaine(taille_ligne,6,ligne_ptr);
+      if(!input.nom)
 	input.nom=alloc_chaine(6,0,"NoName");
       continue;
       /* Une ligne ne peut etre associé a au plus un champs, pas la peine d'évaluer d'autres expressions.*/
     }
         
     /*------ TYPE -------*/
-    if(est_valide(!input.type,file,*ligne_ptr,"TYPE: %ms",true,"TSP","TYPE non géré."))
+    if(est_valide(!input.type,file,ligne_ptr,"TYPE: %ms",true,"TSP","TYPE non géré."))
     {
       /*Si une erreur est survenue, est_valide a lancé un exit.*/
-      input.type=alloc_chaine(taille_ligne,6,*ligne_ptr);
+      input.type=alloc_chaine(taille_ligne,6,ligne_ptr);
       continue;
     }
 
     /*------ COMMENT ------*/
-    if(est_valide(!input.commentaire,file,*ligne_ptr,"COMMENT: %42",false,NULL,NULL))
+    if(est_valide(!input.commentaire,file,ligne_ptr,"COMMENT: %42",false,NULL,NULL))
     {
-      input.commentaire=alloc_chaine(taille_ligne,9,*ligne_ptr);
-      if(!strcmp('\0',input.commentaire))
+      input.commentaire=alloc_chaine(taille_ligne,9,ligne_ptr);
+      if(!input.commentaire)
 	input.commentaire=alloc_chaine(6,0,"NoComm");
       continue;
     }
 	  
     /*------ DIMENSION ------ AMELIORATION POSSIBLE EN MODIFIANT est_valide.*/
-    if(est_valide(!input.dimension,file,*ligne_ptr,"DIMENSION: %42",false,NULL,NULL))
+    if(est_valide(!input.dimension,file,ligne_ptr,"DIMENSION: %42",false,NULL,NULL))
     {
-      sscanf(*ligne_ptr,"DIMENSION: %d",&input.dimension);
+      sscanf(ligne_ptr,"DIMENSION: %d",&input.dimension);
       if(input.dimension<=0)
       {
 	error(0,0,"Dimension incorrecte");
-	free_erreur(file,*ligne_ptr);
+	free_erreur(file,ligne_ptr);
       }
       continue;
     }
 
     /*------ EDGE WEIGHT TYPE ------*/
-    if(est_valide(!input.edge_weight_type,file,*ligne_ptr,"EDGE_WEIGHT_TYPE: %ms",true,"EXPLICIT","EDGE_WEIGHT_TYPE non géré."))
+    if(est_valide(!input.edge_weight_type,file,ligne_ptr,"EDGE_WEIGHT_TYPE: %ms",true,"EXPLICIT","EDGE_WEIGHT_TYPE non géré."))
     {
-      input.edge_weight_type=alloc_chaine(taille_ligne,18,*ligne_ptr);
+      input.edge_weight_type=alloc_chaine(taille_ligne,18,ligne_ptr);
       continue;
     }
 
     /*------ EDGE WEIGHT FORMAT ------*/
-    if(est_valide(!input.edge_weight_format,file,*ligne_ptr,"EDGE_WEIGHT_FORMAT: %ms",true,"FULL_MATRIX","EDGE_WEIGHT_FORMAT non géré."))
+    if(est_valide(!input.edge_weight_format,file,ligne_ptr,"EDGE_WEIGHT_FORMAT: %ms",true,"FULL_MATRIX","EDGE_WEIGHT_FORMAT non géré."))
     {
-      input.edge_weight_type=alloc_chaine(taille_ligne,18,*ligne_ptr);
+      input.edge_weight_type=alloc_chaine(taille_ligne,18,ligne_ptr);
       continue;
     }
 
     /*------ DISPLAY_DATA_TYPE ------*/
-    if(est_valide(!input.display_data_type,file,*ligne_ptr,"DISPLAY_DATA_TYPE: %ms",true,"TWOD_DISPLAY","DISPLAY_DATA_TYPE non géré."))
+    if(est_valide(!input.display_data_type,file,ligne_ptr,"DISPLAY_DATA_TYPE: %ms",true,"TWOD_DISPLAY","DISPLAY_DATA_TYPE non géré."))
     {
-      input.edge_weight_type=alloc_chaine(taille_ligne,20,*ligne_ptr);
+      input.display_data_type=alloc_chaine(taille_ligne,19,ligne_ptr);
       continue;
     }
 
     /*------ PARTIE SECTION ------*/
-    bool_section = !strncmp("EDGE_WEIGHT_SECTION",*ligne_ptr,19);
-    if( bool_section || !strncmp("DISPLAY_DATA_SECTION",*ligne_ptr,20) )
+    bool_section = !strncmp("EDGE_WEIGHT_SECTION",ligne_ptr,19);
+    if( bool_section || !strncmp("DISPLAY_DATA_SECTION",ligne_ptr,20) )
     {
       /* Pour parser les différents champs de données, il faut que certains champs soient présents 
        * sinon, on affiche un message d'erreur */
-      if(bool_section && !input.display_data_type && !input.dimension)
+      if(bool_section && input.display_data_type && input.dimension)
       {
 	parsing_display(file,ligne_ptr,taille_alloc);
 	continue;
       }
-      else if(!input.dimension && !input.edge_weight_type && !input.edge_weight_matrix && !input.edge_weight_format)
+      else if(input.dimension && input.edge_weight_type && input.edge_weight_matrix && input.edge_weight_format)
       {
 	parsing_matrice(file,ligne_ptr,taille_alloc);
 	continue;
       }
       error(0,0,"Champs manquant avant section_part, ou section_part redondante.");
-      free_erreur(file,*ligne_ptr);
+      free_erreur(file,ligne_ptr);
     }
 
     /*------ EOF OPTIONNEL ------*/
-    if(!strncmp("EOF",*ligne_ptr,3))
+    if(!strncmp("EOF",ligne_ptr,3))
        break;
     
     /*------ ERREUR ------ */
     printf("Erreur avec : %s",*ligne_ptr);
     error(0,0,"Champs redondant,inconnu ou dimension incorrecte.");
-    free_erreur(file,*ligne_ptr);
+    free_erreur(file,ligne_ptr);
   }
   /* On est sorti du while car on a fini de lire, ou a la rencontre d'un "EOF"
    * Si le champ edge_weight_matrix a été rencontré, alors le parsing est réussi.*/
   if(!input.edge_weight_matrix)
   {
     affiche_erreur();
-    free_erreur(file,*ligne_ptr);
+    free_erreur(file,ligne_ptr);
   }
 }
 
@@ -294,55 +294,61 @@ static void affectation_display_data(int indice,double x,double y)
   input.display_data[indice][1]=y;
 }
   
-static void parsing_display(FILE* file,char** ligne_ptr,size_t* taille_alloc)
+void affichage(int a){
+  printf("%d",a);
+}
+
+static void parsing_display(FILE* file,char* ligne_ptr,size_t taille_alloc)
 {
   int taille_ligne,nombre_affectation;
   int taille= input.dimension;
-  double * position_x;
-  double* position_y;
-  int * numero_ville;
+  double position_x;
+  double position_y;
+  int numero_ville;
 
   /* On initialise la matrice display_data a NULL pour les tests d'affectation.*/
   input.display_data= calloc(taille,sizeof(double*));
   for(int i=0;i<taille;i++)
   {
-    if((taille_ligne=getline(ligne_ptr,taille_alloc,file)) != EOF)
+    if((taille_ligne=getline(&ligne_ptr,&taille_alloc,file)) != EOF)
     {
-      nombre_affectation=sscanf(*ligne_ptr,"%d %lf %lf",numero_ville,position_x,position_y);
+      nombre_affectation=sscanf(ligne_ptr,"%d %lf %lf",&numero_ville,&position_x,&position_y);
       
       /* Si il y a moins de 3 affectations, erreur de lecture */
       if(nombre_affectation<3)
       {
 	error(0,0,"Format de la display_section incorrect.");
-	free_erreur(file,*ligne_ptr);
+	free_erreur(file,ligne_ptr);
       }
       
-      if(*numero_ville<1 || *numero_ville>taille || !input.display_data[*(numero_ville)-1])
+      if(numero_ville<1 || numero_ville > taille || input.display_data[numero_ville-1])
+      /* input.display_data[numero_ville-1] : On verifie qu'il n'y a pas deja eu d'affectation.*/
       {
+   
 	error(0,0,"Erreur display_section : ville avec le meme numéro ou numéro incorrect.");
-	free_erreur(file,*ligne_ptr);
+	free_erreur(file,ligne_ptr);
       }
 
       /* L'affectation realisé via sscanf est donc valide. */
-      affectation_display_data((*numero_ville)-1,(*position_x),(*position_y));
+      affectation_display_data(numero_ville-1,position_x,position_y);
     }
     affiche_erreur();
-    free_erreur(file,*ligne_ptr);
+    free_erreur(file,ligne_ptr);
   }
 }
 
-static void parsing_matrice(FILE* file,char** ligne_ptr,size_t* taille_alloc)
+static void parsing_matrice(FILE* file,char* ligne_ptr,size_t taille_alloc)
 {
   char* ptr_debut_double;
   char** ptr_fin_double;
-  int taille_ligne;
+  ssize_t taille_ligne;
   int taille=input.dimension;
   
   for(int i=0;i<taille;i++)
   {
-    if(taille_ligne=getline(ligne_ptr,taille_alloc,file) != EOF)
+    if(taille_ligne=getline(&ligne_ptr,&taille_alloc,file) != EOF)
     {
-      ptr_debut_double=*ligne_ptr;
+      ptr_debut_double=ligne_ptr;
       ptr_fin_double=NULL;
       for(int j=0;i<taille;i++)
       {
@@ -350,14 +356,14 @@ static void parsing_matrice(FILE* file,char** ligne_ptr,size_t* taille_alloc)
 	if(ptr_debut_double==*ptr_fin_double || input.edge_weight_matrix[i][j]<0)
 	{
 	  error(0,0,"Erreur matrice: distance ou format incorrect.");
-	  free_erreur(file,*ligne_ptr);
+	  free_erreur(file,ligne_ptr);
 	}
 	/* On passe a la valeur suivante,en décalant le pointeur de debut. */
 	ptr_debut_double=*ptr_fin_double;
       }
     }
     affiche_erreur();
-    free_erreur(file,*ligne_ptr);
+    free_erreur(file,ligne_ptr);
   }
 }
     
