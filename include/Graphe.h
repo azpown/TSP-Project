5860b75dfe032485b79c5cd4148253568886be57
azpown <azowned@hotmail.fr>
2014-04-15 00:56:47 +0200

    ajout documentation graphe

--- include/Graphe.h
+++ include/Graphe.h
@@ -1,13 +1,50 @@
+/**
+ * \file Graphe.h
+ * \author Delmas Rémi
+ * \brief Module de manipulation de la matrice de distance.
+*/
+
 #ifndef GRAPHE
 #define GRAPHE
 
 typedef struct graphe * Graphe;
 
+
+/**
+ * \brief Fonction de création d'une instance de Graphe.
+ * \param len Dimension de la matrice de distance, strictement positive.
+ * \param mat Matrice 2D contenant les distances entre chaque villes.
+ * \post L'instance doit etre free avec free_graphe
+*/
 Graphe cree_graphe(int len,double** mat);
+
+/**
+ *\brief Fonction de destruction d'une instance Graphe.
+*/
 void free_graphe(Graphe graphe);
+
+/**
+ * \brief Affiche une contenu instance de type Graphe sous la forme :
+ * Affichage de la structure graphe:
+ * Taille : <len>
+ * Matrice :
+ * <numVille>- <distance00>- <distance01>- ...
+ * (deux décimale affichées uniquement.)
+ * \param input Instance de type Graphe à afficher.
+*/
 void afficher_graphe(Graphe g);
+
+/**
+ * \brief Retourne la distance entre deux villes d'indice s1 et s2 dans graphe.
+ * L'ordre n'a pas d'importance. 
+*/
 double distance_ville(Graphe graphe,int s1,int s2);
+
+/**
+ * \brief Retourne le nombre de ville dans l'instance graphe.
+*/
 int get_taille(Graphe graphe);
+
 double get_double(Graphe g,int i,int j);
 
 #endif
--- include/Input.h
+++ include/Input.h
@@ -1,37 +1,48 @@
 #ifndef INPUT
 #define INPUT
 
+/**
+ * \file Input.h
+ * \author Delmas Rémi
+ * \brief Module pour le parsing de fichier TSP
+*/
+
 typedef struct input *Input;
 
 /**
- * \b Affiche une instance de type Input.
+ * \brief Affiche une instance de type Input.
  * \param input instance de type Input à afficher.
 */
 void print_input_data(Input input);
 
 /**
- *\b Fonction de parsing d'un fichier TSP.
+ *\brief Fonction de parsing d'un fichier TSP.
  *\param nom_file chemin du fichier .tsp à parser.
- *\pre présentement, il n'y a que les instances de type FULL_MATRIX qui sont gérées
+ *\pre Présentement, il n'y a que les instances de type FULL_MATRIX qui sont gérées
  * un message d'erreur est affiché et la mémoire est libéré en cas de format incorrect.
+ * La matrice de donnée doit etre symétrique et contenir des valeurs strictement  positives
  *\post l'instance Input doit etre free via free_input.
 */
 Input open_TSP_file(char* nom_file);
+
+/**
+ *\brief Fonction de destruction d'une instance Input.
+*/
 void free_input(Input input);
 
 /**
- *\b Retourne le nom du fichier .tsp qui a servi à générer l'instance d'Input passée en paramètre.
+ *\brief Retourne le nom du fichier .tsp qui a servi à générer l'instance d'Input passée en paramètre.
 */
 char* get_nom_file(Input input);
 
 /**
- *\b Retourne le champs nom du fichier .tsp qui a servi à générer l'instance d'Input en paramètre.
+ *\brief Retourne le champs nom du fichier .tsp qui a servi à générer l'instance d'Input en paramètre.
  *\pre si ce champs est vide ou non présent, la valeur renvoyée est "NoName".
 */
 char* get_nom(Input input);
 
 /**
- *\b Retourne le champs type (ex: TSP) du fichier .tsp qui a servi à générer l'instance d'Input en paramètre.
+ *\brief Retourne le champs type (ex: TSP) du fichier .tsp qui a servi à générer l'instance d'Input en paramètre.
 */
 char* get_type(Input input);
 
@@ -42,13 +53,13 @@ char* get_type(Input input);
 char* get_commentaire(Input input);
 
 /**
- *\b Retourne la dimension (Pas de borne sup) de la matrice du fichier .tsp qui a servi à générer l'instance d'Input en paramètre.
+ *\brief Retourne la dimension (Pas de borne sup) de la matrice du fichier .tsp qui a servi à générer l'instance d'Input en paramètre.
  *\pre la dimension doit etre strictement supérieure a 0.
 */
 int   get_dimension(Input input);
 
 /**
- *\b Retourne le champs edge_weight_type (ex: EXPLICIT) du fichier .tsp qui a servi à générer l'instance d'Input en paramètre.
+ *\brief Retourne le champs edge_weight_type (ex: EXPLICIT) du fichier .tsp qui a servi à générer l'instance d'Input en paramètre.
 */
 char* get_edge_weight_type(Input input);
 
@@ -58,18 +69,18 @@ char* get_edge_weight_type(Input input);
 char* get_edge_weight_format(Input input);
 
 /**
- *\b Retourne le champs display_data_type (ex: TWOD_DISPLAY) du fichier .tsp qui a servi à générer l'instance d'Input en paramètre.
+ *\brief Retourne le champs display_data_type (ex: TWOD_DISPLAY) du fichier .tsp qui a servi à générer l'instance d'Input en paramètre.
 */
 char* get_display_data_type(Input input);
 
 /**
- *\b Retourne la matrice de travail du fichier .tsp qui a servi à générer l'instance d'Input en paramètre.
+ *\brief Retourne la matrice de travail du fichier .tsp qui a servi à générer l'instance d'Input en paramètre.
  *\pre les valeurs contenues dans la matrice doivent être strictements supérieures a 0.
 */
 double** get_edge_weight_matrix(Input input);
 
 /**
- *\b Retourne la matrice d'affichage du fichier .tsp qui a servi à générer l'instance d'Input en paramètre.
+ *\brief Retourne la matrice d'affichage du fichier .tsp qui a servi à générer l'instance d'Input en paramètre.
 */
 double** get_display_data(Input input);
 
